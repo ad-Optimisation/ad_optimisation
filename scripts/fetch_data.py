8df58f00492988183e2207087afe80a66afaa800
Fisseha Estifanos <fisseha.137@gmail.com>
2022-11-01 19:19:57 +0000

    added documentation

--- scripts/fetch_data.py
+++ scripts/fetch_data.py
@@ -27,5 +27,21 @@ def fetch_data(_bucket_name: str = defs.bucket_name,
 
 
 def extract_data(file_name: str, from_path: str, to_path: str):
+    """
+    A function to extract data from S3 bucket
+
+    Parameters
+    ----------
+    file_name: str
+    Path to the file to be downloaded
+    from_path: str
+    Path where the file will be saved
+    to_path: str
+    Path where the file will be saved
+
+    Returns
+    -------
+    None
+    """
     with zipfile.ZipFile(from_path + file_name, 'r') as zip_ref:
         zip_ref.extractall(to_path)
