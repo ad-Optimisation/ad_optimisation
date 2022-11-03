ab2ca8b8933c890ba1fa485aecb52e4bb8a55dfa
Fisseha Estifanos <fisseha.137@gmail.com>
2022-11-03 07:58:55 +0000

    root paths added

--- scripts/defaults.py
+++ scripts/defaults.py
@@ -11,3 +11,6 @@
 bucket_name = "10ac-batch-6"
 from_path = "data/w11/Challenge_Data.zip"
 to_path = "original_data/Challenge_Data.zip"
+
+root_challenge_path = "/home/Challenge_Data"
+root_assets_path = "/Assets/"
