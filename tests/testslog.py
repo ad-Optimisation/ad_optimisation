8b7c89ee4d066ac3fdf2f433df9014918e97142a
Fisseha Estifanos <fisseha.137@gmail.com>
2022-11-03 14:04:48 +0000

    free run: pep8

--- tests/testslog.py
+++ tests/testslog.py
@@ -1,12 +1,12 @@
 from tkinter.filedialog import test
 import unittest
 import pandas as pd
+
+
 class TestGetInformations(unittest.TestCase):
     def test_load_store(self):
-       self.assertIsInstance(df,pd.DataFrame)
-       
-   
-        
-        
+        self.assertIsInstance(df, pd.DataFrame)
+
+
 if __name__ == "__main__":
     unittest.main()
