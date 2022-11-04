ab8bd69574fb02f539889abbae7ef765e258f0b4
kebishaa <kbkibatu@gmail.com>
2022-11-04 10:53:40 +0300

    added some code base

--- scripts/frame_extraction.py
+++ scripts/frame_extraction.py
@@ -72,4 +72,61 @@ def is_status_complete(self, passed_driver) -> bool:
                     return True
 
         return False
+    
+def terminate(process: Popen[bytes]) -> None:
+        '''
+        Function to stop/terminate a process.
+        '''
+        if process.poll() is None:
+            call("taskkill /F /T /PID " + str(process.pid))
+
+
+def crop_video(filename: str, x_pos: float = 0, y_pos: float = 70, width: float = 650, height: float = 970) -> None:
+        '''
+        Function to crop a video with given location and size specific parameters.
+        '''
+        print(filename)
+        input_video = ffmpeg.input(f"{filename}.mkv")
+        cropped_video = ffmpeg.crop(input_video, x_pos, y_pos, width, height)
+        output_video = ffmpeg.output(cropped_video, f"{filename}_cropped.mkv")
+        ffmpeg.run(output_video)
+
+def _imitate_engagement(self, ad_size: Tuple[float, float]) -> None:
+        '''
+        Function to imitate a given engagement type.
+        '''
+        center = (ad_size[0]/2, self.browser_edges[0] + (ad_size[1]/2))
+
+        if self.engagement_type == "tap":
+            pyautogui.moveTo(center[0], center[1], duration=1)
+            pyautogui.leftClick()
+
+        elif self.engagement_type == "swipe right":
+            pyautogui.moveTo(center[0], center[1], duration=1)
+            pyautogui.dragRel(center[0], 0, duration=1)
+
+        elif self.engagement_type == "swipe left":
+            pyautogui.moveTo(center[0], center[1], duration=1)
+            pyautogui.dragRel(-center[0], 0, duration=1)
+
+        elif self.engagement_type == "tap and hold":
+            pyautogui.moveTo(center[0], center[1], duration=1)
+            pyautogui.click()
+
+        elif self.engagement_type == "scrub":
+            pyautogui.moveTo(center[0] - (1/2 * center[0]),
+                             center[1] - (2/3 * center[1]), duration=0.2)
+            pyautogui.dragRel(center[0], 0, duration=0.2)
+            pyautogui.dragRel(-center[0], (1/3 * center[1]), duration=0.2)
+            pyautogui.dragRel(center[0], 0, duration=0.2)
+            pyautogui.dragRel(-center[0], (1/3 * center[1]), duration=0.2)
+            pyautogui.dragRel(center[0], 0, duration=0.2)
+def generate_frames(self) -> None:
+        '''
+        Function to generate creative start and end frames.
+        '''
+        driver = webdriver.Chrome(
+            options=self.opt, desired_capabilities=self.capabilities, )
+        driver.maximize_window()
+          
          
