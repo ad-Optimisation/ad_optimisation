6f29c0d9946a8f880059f14ca06a284af28b233b
Fisseha Estifanos <fisseha.137@gmail.com>
2022-11-05 11:08:08 +0000

    minor mod

--- scripts/dataFetcher.py
+++ scripts/dataFetcher.py
@@ -13,7 +13,7 @@ def fetch_data(_bucket_name: str = defs.bucket_name,
     A function to fetch data from S3 bucket
 
     Parameters
-    ----------
+    =--------=
     _bucket_name: str
         Name of the S3 bucket
     _from_path: str
@@ -22,7 +22,7 @@ def fetch_data(_bucket_name: str = defs.bucket_name,
         Path where the file will be saved
 
     Returns
-    -------
+    =-----=
     None
     """
     s3 = boto3.client('s3')
@@ -34,7 +34,7 @@ def extract_data(file_name: str, from_path: str, to_path: str):
     A function to extract data from S3 bucket
 
     Parameters
-    ----------
+    =--------=
     file_name: str
         Path to the file to be downloaded
     from_path: str
@@ -43,7 +43,7 @@ def extract_data(file_name: str, from_path: str, to_path: str):
         Path where the file will be saved
 
     Returns
-    -------
+    =-----=
         None
     """
     with zipfile.ZipFile(from_path + file_name, 'r') as zip_ref:
